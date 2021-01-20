8bcfc6d44c72ee78f708e2208a53046513983be2
q350241636 <350241636@qq.com>
2021-01-20 16:52:51 +0800

    一大波修改

--- src/main/java/com/blog/api/service/PermissionService.java
+++ src/main/java/com/blog/api/service/PermissionService.java
@@ -1,10 +1,13 @@
 package com.blog.api.service;
 
 import com.blog.api.model.Permission;
+import com.blog.api.model.base.BaseModel;
 import com.blog.api.repo.PermissionRepository;
 import com.blog.api.repo.RoleRepository;
 import com.blog.api.service.base.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -36,6 +39,11 @@ public class PermissionService extends BaseService {
 //        var p=new Permission();
 //        p.setFunctionUrl("/api/user/getAll");
 //        return p;
+        return null;
+    }
+
+    @Override
+    public Page getPageList(BaseModel params, Pageable pageable) {
         return null;
     }
 }
